e7018b603bc79f031d9fe63cf6597614d9cc51b3
Onyiaji Netochukwu <onyiajineto@gmail.com>
2017-03-21 19:17:17 +0100

    working version of holiday api

--- holidayapi.js
+++ holidayapi.js
@@ -1,30 +1,15 @@
-var HolidayAPI = require('node-holidayapi');
-var hapi = new HolidayAPI('85110baa-372b-4698-975c-b07394fb7a53').v1;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://holidayapi.com/v1/holidays?country=US&year=2017&key=85110baa-372b-4698-975c-b07394fb7a53", false);
+xhr.send();
 
-var parameters = {
-  // Required
-  country: 'US',
-  year:    2017,
-  // Optional
-  // month:    7,
-  // day:      4,
-  // previous: true,
-  // upcoming: true,
-  // public:   true,
-  // pretty:   true,
-};
+response = JSON.parse(xhr.response);
 
-hapi.holidays(parameters, function (err, data) {
-   if (!err){
-            console.log(data.status)
-            console.log(data.holidays)
-         // var data = d
-          //var name = data.name;
-         // var date = data.date;
-         // var observed = data.observed;
+for (var date in response.holidays) {
+  for(var i = 0; i < response.holidays[date].length; i++){
+    console.log(response.holidays[date][i].name);
+     console.log(response.holidays[date][i].date);
+  }
+  
+  
+}
 
-         // console.log('The holiday is called ' + name)
-         // console.log('The date  is' + date)
-         // console.log('It is observed on ' + observed )
-   }
-});
